ec3811368ad29213afdeeb6e5ae430805e5cd68d
awoyuki <oresanjo123>
2023-04-10 01:29:36 +0700

    UPDATE MAP/CHANGE MAP + OPTIMZE FOR VR

---
+++ Plugins/VRExpansionPlugin/VRExpansionPlugin/Intermediate/Build/Win64/UE4/Inc/VRExpansionPlugin/VRPlayerController.generated.h
@@ -0,0 +1,257 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+class APlayerCameraManager;
+#ifdef VREXPANSIONPLUGIN_VRPlayerController_generated_h
+#error "VRPlayerController.generated.h already included, missing '#pragma once' in VRPlayerController.h"
+#endif
+#define VREXPANSIONPLUGIN_VRPlayerController_generated_h
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_SPARSE_DATA
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_RPC_WRAPPERS
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_RPC_WRAPPERS_NO_PURE_DECLS
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_EVENT_PARMS \
+	struct VRBasePlayerController_eventOnCameraManagerCreated_Parms \
+	{ \
+		APlayerCameraManager* CameraManager; \
+	};
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_CALLBACK_WRAPPERS
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAVRBasePlayerController(); \
+	friend struct Z_Construct_UClass_AVRBasePlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AVRBasePlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(AVRBasePlayerController)
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_INCLASS \
+private: \
+	static void StaticRegisterNativesAVRBasePlayerController(); \
+	friend struct Z_Construct_UClass_AVRBasePlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AVRBasePlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(AVRBasePlayerController)
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVRBasePlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVRBasePlayerController) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVRBasePlayerController); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVRBasePlayerController); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVRBasePlayerController(AVRBasePlayerController&&); \
+	NO_API AVRBasePlayerController(const AVRBasePlayerController&); \
+public:
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVRBasePlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVRBasePlayerController(AVRBasePlayerController&&); \
+	NO_API AVRBasePlayerController(const AVRBasePlayerController&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVRBasePlayerController); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVRBasePlayerController); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVRBasePlayerController)
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_PRIVATE_PROPERTY_OFFSET
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_PROLOG \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_EVENT_PARMS
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_PRIVATE_PROPERTY_OFFSET \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_SPARSE_DATA \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_RPC_WRAPPERS \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_CALLBACK_WRAPPERS \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_INCLASS \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_PRIVATE_PROPERTY_OFFSET \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_SPARSE_DATA \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_RPC_WRAPPERS_NO_PURE_DECLS \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_CALLBACK_WRAPPERS \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_INCLASS_NO_PURE_DECLS \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_17_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> VREXPANSIONPLUGIN_API UClass* StaticClass<class AVRBasePlayerController>();
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_SPARSE_DATA
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_RPC_WRAPPERS
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_RPC_WRAPPERS_NO_PURE_DECLS
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAVRPlayerController(); \
+	friend struct Z_Construct_UClass_AVRPlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AVRPlayerController, AVRBasePlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(AVRPlayerController)
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_INCLASS \
+private: \
+	static void StaticRegisterNativesAVRPlayerController(); \
+	friend struct Z_Construct_UClass_AVRPlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AVRPlayerController, AVRBasePlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(AVRPlayerController)
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVRPlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVRPlayerController) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVRPlayerController); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVRPlayerController); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVRPlayerController(AVRPlayerController&&); \
+	NO_API AVRPlayerController(const AVRPlayerController&); \
+public:
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVRPlayerController(AVRPlayerController&&); \
+	NO_API AVRPlayerController(const AVRPlayerController&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVRPlayerController); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVRPlayerController); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVRPlayerController)
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_PRIVATE_PROPERTY_OFFSET
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_38_PROLOG
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_PRIVATE_PROPERTY_OFFSET \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_SPARSE_DATA \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_RPC_WRAPPERS \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_INCLASS \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_PRIVATE_PROPERTY_OFFSET \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_SPARSE_DATA \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_RPC_WRAPPERS_NO_PURE_DECLS \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_INCLASS_NO_PURE_DECLS \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_41_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> VREXPANSIONPLUGIN_API UClass* StaticClass<class AVRPlayerController>();
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_SPARSE_DATA
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_RPC_WRAPPERS
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_RPC_WRAPPERS_NO_PURE_DECLS
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUVRLocalPlayer(); \
+	friend struct Z_Construct_UClass_UVRLocalPlayer_Statics; \
+public: \
+	DECLARE_CLASS(UVRLocalPlayer, ULocalPlayer, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UVRLocalPlayer)
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_INCLASS \
+private: \
+	static void StaticRegisterNativesUVRLocalPlayer(); \
+	friend struct Z_Construct_UClass_UVRLocalPlayer_Statics; \
+public: \
+	DECLARE_CLASS(UVRLocalPlayer, ULocalPlayer, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UVRLocalPlayer)
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UVRLocalPlayer(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UVRLocalPlayer) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UVRLocalPlayer); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UVRLocalPlayer); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UVRLocalPlayer(UVRLocalPlayer&&); \
+	NO_API UVRLocalPlayer(const UVRLocalPlayer&); \
+public:
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UVRLocalPlayer(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UVRLocalPlayer(UVRLocalPlayer&&); \
+	NO_API UVRLocalPlayer(const UVRLocalPlayer&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UVRLocalPlayer); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UVRLocalPlayer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UVRLocalPlayer)
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_PRIVATE_PROPERTY_OFFSET
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_73_PROLOG
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_PRIVATE_PROPERTY_OFFSET \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_SPARSE_DATA \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_RPC_WRAPPERS \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_INCLASS \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_PRIVATE_PROPERTY_OFFSET \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_SPARSE_DATA \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_RPC_WRAPPERS_NO_PURE_DECLS \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_INCLASS_NO_PURE_DECLS \
+	FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_76_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class VRLocalPlayer."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> VREXPANSIONPLUGIN_API UClass* StaticClass<class UVRLocalPlayer>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FireTrainingVR_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
